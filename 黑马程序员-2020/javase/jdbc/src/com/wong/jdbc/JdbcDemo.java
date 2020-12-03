0e5a759c14df1ef33577f0dc02a52290c25e36e3
JosephWong <joseph_wong1993@foxmail.com>
2020-12-03 15:31:27 +0800

    Study MySQL JDBC

---
+++ 黑马程序员-2020/javase/jdbc/src/com/wong/jdbc/JdbcDemo.java
@@ -0,0 +1,11 @@
+package com.wong.jdbc;
+
+/**
+ * JDBC：Java连接数据库技术
+ * Java语言和数据库去的连接，对数据进行CRUD
+ * Sun公司提供JDBC开发API，类库（类，接口...）java.sql包中
+ * 可以开发任意关系数据库，我们程序人员写代码不会改变
+ */
+public class JdbcDemo {
+
+}
