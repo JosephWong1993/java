8f8c548beb88e3b5871f3b337316045fc85fbc9c
JosephWong <joseph_wong1993@foxmail.com>
2020-12-29 11:52:30 +0800

    Study Spring AOP CRUD Demo

--- 黑马程序员-2020/spring/spring_aop_advice_annotation_2/src/test/java/com/wong/service/AccountServiceTest.java
+++ 黑马程序员-2020/spring/spring_aop_advice_annotation_2/src/test/java/com/wong/service/AccountServiceTest.java
@@ -8,22 +8,22 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { SpringConfig.class })
+@ContextConfiguration(classes = {SpringConfig.class})
 public class AccountServiceTest {
-    
+
     @Autowired
     private AccountService accountService;
-    
+
     @Test
     public void save() {
         accountService.save();
     }
-    
+
     @Test
     public void update() {
         accountService.update(123);
     }
-    
+
     @Test
     public void delete() {
         accountService.delete();
