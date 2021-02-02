7e6deb66e6965ff7d8f7d80a662df80490cb760b
JosephWong <joseph_wong1993@foxmail.com>
2021-02-02 20:52:13 +0800

    Study JavaSE 设计模式 单例设计模式

---
+++ 黑马程序员-2020/javase/javase13/src/main/java/com/wong/design/single01/Demo.java
@@ -0,0 +1,11 @@
+package com.wong.design.single01;
+
+/**
+ * java.lang.Runtime 可以执行命令 windows命令
+ * exec()
+ */
+public class Demo {
+    public static void main(String[] args) {
+        Single s = Single.getInstance();
+    }
+}
