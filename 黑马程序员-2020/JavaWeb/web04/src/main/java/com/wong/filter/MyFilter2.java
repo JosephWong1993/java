5bbc2c34ae7c7c0c84e5ad0d2ce96b878a293182
JosephWong <joseph_wong1993@foxmail.com>
2021-03-02 21:03:40 +0800

    Study JSP Filter

--- 黑马程序员-2020/JavaWeb/web04/src/main/java/com/wong/filter/MyFilter2.java
+++ 黑马程序员-2020/JavaWeb/web04/src/main/java/com/wong/filter/MyFilter2.java
@@ -8,13 +8,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import java.io.IOException;
 
-/**
- * 过滤器的快速入门
- * 1：定义类实现接口 Filter
- * 2：重写接口中的抽象方法
- * 3：web.xml配置
- */
-public class MyFilter1 implements Filter {
+public class MyFilter2 implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     
@@ -23,8 +17,7 @@ public class MyFilter1 implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        System.out.println("我是过滤器1");
-        //放行
+        System.out.println("我是过滤器2");
         chain.doFilter(request, response);
     }
     
