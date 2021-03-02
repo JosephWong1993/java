c803ac0da42a0557a14df9a81d43a97c689b4a42
JosephWong <joseph_wong1993@foxmail.com>
2021-03-02 21:21:51 +0800

    Study JSP Filter对象生命周期 对象初始化init方法

--- 黑马程序员-2020/JavaWeb/web04/src/main/java/com/wong/filter/MyFilter1.java
+++ 黑马程序员-2020/JavaWeb/web04/src/main/java/com/wong/filter/MyFilter1.java
@@ -3,6 +3,7 @@ package com.wong.filter;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -13,18 +14,31 @@ import java.io.IOException;
  * 1：定义类实现接口 Filter
  * 2：重写接口中的抽象方法
  * 3：web.xml配置
+ * <p>
+ * 过滤器对象的生命周期
+ * 1：对象初始化创建，调用方法init
+ * 方法参数 FilterConfig
+ * Tomcat引擎创建过滤器对象，并调用方法init传递参数
+ * Tomcat启动的时候，创建过滤器对象
+ * <p>
+ * 方法参数 FilterConfig，过滤器配置对象
+ * 可以获取到过滤器的名字等待
+ * 方法：getServletContext()获取到最大的域对象
  */
 public class MyFilter1 implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-    
+        System.out.println("过滤器对象被创建");
+        ServletContext servletContext = filterConfig.getServletContext();
+        System.out.println(servletContext);
     }
     
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         System.out.println("我是过滤器1");
-        //放行
+        //        response = null;
+        //放行s
         chain.doFilter(request, response);
     }
     
