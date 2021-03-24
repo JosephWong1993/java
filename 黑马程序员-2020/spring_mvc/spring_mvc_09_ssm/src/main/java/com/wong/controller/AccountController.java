5b46671b892fb63f333084d4ee1038e907e74019
JosephWong <joseph_wong1993@foxmail.com>
2021-03-24 14:27:51 +0800

    study spring MVC

--- 黑马程序员-2020/spring_mvc/spring_mvc_09_ssm/src/main/java/com/wong/controller/AccountController.java
+++ 黑马程序员-2020/spring_mvc/spring_mvc_09_ssm/src/main/java/com/wong/controller/AccountController.java
@@ -3,6 +3,7 @@ package com.wong.controller;
 import com.wong.pojo.Account;
 import com.wong.service.AccountService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,15 +19,33 @@ import java.util.List;
 public class AccountController {
     //依赖注入AccountService对象
     private AccountService accountService;
-
+    
     public AccountController(AccountService accountService) {
         this.accountService = accountService;
     }
-
+    
+    /**
+     * 改
+     */
+    @RequestMapping("update")
+    public String update(@RequestBody Account account) {
+        //调用Service层的修改方法
+        accountService.update(account);
+        return "修改成功";
+    }
+    
+    /**
+     * 查（根据id 查询账户对象）
+     */
+    @RequestMapping("findById")
+    public Account findById(Integer id) {
+        //调用service， 获取指定id 对应的account对象
+        Account account = accountService.findById(id);
+        return account;
+    }
+    
     /**
      * 查（查询所有账户对象）
-     *
-     * @return
      */
     @RequestMapping("findAll")
     public List<Account> findAll() {
