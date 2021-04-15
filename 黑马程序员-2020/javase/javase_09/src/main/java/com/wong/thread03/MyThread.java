2a941ef69b6d4f9944033e23be5653836f27ad60
JosephWong <joseph_wong1993@foxmail.com>
2021-04-16 01:12:37 +0800

    study JavaSE Thread

--- 黑马程序员-2020/javase/javase_09/src/main/java/com/wong/thread03/MyThread.java
+++ 黑马程序员-2020/javase/javase_09/src/main/java/com/wong/thread03/MyThread.java
@@ -3,6 +3,7 @@ package com.wong.thread03;
 public class MyThread extends Thread {
     @Override
     public void run() {
-        System.out.println("线程名字::" + getName());
+        Thread t = Thread.currentThread(); //当前线程，就是Thread-0
+        System.out.println("线程名字::" + t.getName());
     }
 }
