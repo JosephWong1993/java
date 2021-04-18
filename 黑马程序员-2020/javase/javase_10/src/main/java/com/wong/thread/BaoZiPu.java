41a1c11a7ecef5c0b986c16f8e2ef54284f1bae1
JosephWong <joseph_wong1993@foxmail.com>
2021-04-18 15:42:41 +0800

    study JavaSE Thread

--- 黑马程序员-2020/javase/javase_10/src/main/java/com/wong/thread/BaoZiPu.java
+++ 黑马程序员-2020/javase/javase_10/src/main/java/com/wong/thread/BaoZiPu.java
@@ -8,10 +8,35 @@ import lombok.Setter;
  * 被线程调用的
  */
 public class BaoZiPu {
+
+    private int count;
+
+    /**
+     * 定义布尔变量，标志位
+     */
+    private boolean flag;
+
+    /**
+     * get方法，被消费者线程调用，输出
+     */
+    public int get() {
+        System.out.println("消费第" + count + "个包子");
+        return count;
+    }
+
     /**
      * set方法，被生产者线程调用，++变量
      */
-    @Getter
-    @Setter
-    private int count;
+    public void set() {
+        count++;
+        System.out.println("生产第" + count + "个包子");
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
 }
---
+++ 黑马程序员-2020/javase/javase_10/src/main/java/com/wong/thread02/BaoZiPu.java
@@ -0,0 +1,50 @@
+package com.wong.thread02;
+
+/**
+ * 资源对象，包子
+ * 被线程调用的
+ */
+public class BaoZiPu {
+
+    private int count;
+
+    /**
+     * 定义布尔变量，标志位
+     */
+    private boolean flag;
+
+    /**
+     * get方法，被消费者线程调用，输出
+     */
+    public synchronized void get() {
+        //判断标志位，是否允许消费
+        if (flag == false) {//没有包子，等，不能消费
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("消费第" + count + "个包子");
+        flag = false;
+        this.notifyAll();
+    }
+
+    /**
+     * set方法，被生产者线程调用，++变量
+     */
+    public synchronized void set() {
+        //判断标志位，是否允许生产
+        if (flag == true) {//有包子，不能生产
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        count++;
+        System.out.println("生产第" + count + "个包子");
+        flag = true;
+        this.notifyAll();
+    }
+}
