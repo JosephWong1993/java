85ab28764d976f8b5bb58b22ae5ff185cd1b483c
JosephWong <joseph_wong1993@foxmail.com>
2021-10-28 15:13:32 +0800

    apache POI 向Excel文件写入数据

--- 黑马程序员-2020/06_health/health_parent/health_common/src/test/java/com/wong/test/PoiTest.java
+++ 黑马程序员-2020/06_health/health_parent/health_common/src/test/java/com/wong/test/PoiTest.java
@@ -8,7 +8,10 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 public class PoiTest {
     /**
@@ -29,6 +32,38 @@ public class PoiTest {
                 System.out.println(cell.getStringCellValue());
             }
         }
+        excel.close();
+    }
+
+    //向Excel文件写入数据，并提供客户端客户端下载
+    @Test
+    public void test3() throws IOException {
+        //在内存中创建一个Excel文件对象
+        XSSFWorkbook excel = new XSSFWorkbook();
+        //在表格中创建一个工作表对象
+        XSSFSheet sheet = excel.createSheet("wong");
+        //在工作表中创建行
+        XSSFRow title = sheet.createRow(0);
+        //在行中创建单元格
+        title.createCell(0).setCellValue("编号");
+        title.createCell(1).setCellValue("姓名");
+        title.createCell(2).setCellValue("地址");
+
+        XSSFRow row1 = sheet.createRow(1);
+        row1.createCell(0).setCellValue("u001");
+        row1.createCell(1).setCellValue("小明");
+        row1.createCell(2).setCellValue("北京");
+
+        XSSFRow row2 = sheet.createRow(2);
+        row2.createCell(0).setCellValue("u002");
+        row2.createCell(1).setCellValue("小李");
+        row2.createCell(2).setCellValue("南京");
+
+        //使用输出流将文件写到磁盘
+        OutputStream out = new FileOutputStream("D:\\temp\\wong.xlsx");
+        excel.write(out);
+        out.flush();
+        out.close();
         excel.close();
     }
 }
