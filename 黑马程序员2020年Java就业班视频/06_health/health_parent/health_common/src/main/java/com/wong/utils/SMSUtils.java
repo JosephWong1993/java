d47a87b1f150e8d179c8e3310f9d7984736f7bfb
Joseph Wong <joseph9312@qq.com>
2021-12-08 21:08:09 +0800

    study

--- 黑马程序员2020年Java就业班视频/06_health/health_parent/health_common/src/main/java/com/wong/utils/SMSUtils.java
+++ 黑马程序员2020年Java就业班视频/06_health/health_parent/health_common/src/main/java/com/wong/utils/SMSUtils.java
@@ -8,6 +8,7 @@ import com.aliyuncs.exceptions.ClientException;
 import com.aliyuncs.http.MethodType;
 import com.aliyuncs.profile.DefaultProfile;
 import com.aliyuncs.profile.IClientProfile;
+import com.wong.config.AliyunSmsConfig;
 
 /**
  * 短信发送工具类
@@ -31,8 +32,8 @@ public class SMSUtils {
         final String product = "Dysmsapi";// 短信API产品名称（短信产品名固定，无需修改）
         final String domain = "dysmsapi.aliyuncs.com";// 短信API产品域名（接口地址固定，无需修改）
         // 替换成你的AK
-        final String accessKeyId = "LTAIak3CfAehK7cE";// 你的accessKeyId,参考本文档步骤2
-        final String accessKeySecret = "zsykwhTIFa48f8fFdU06GOKjHWHel4";// 你的accessKeySecret，参考本文档步骤2
+        final String accessKeyId = AliyunSmsConfig.accessKeyId;// 你的accessKeyId,参考本文档步骤2
+        final String accessKeySecret = AliyunSmsConfig.secret;// 你的accessKeySecret，参考本文档步骤2
         // 初始化ascClient,暂时不支持多region（请勿修改）
         IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
         DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
