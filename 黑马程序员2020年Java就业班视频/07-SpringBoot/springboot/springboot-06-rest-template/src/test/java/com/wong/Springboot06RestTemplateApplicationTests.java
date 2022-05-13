cb338b2ec7fb5bdcae23c64583e8f0cfd6ec63b5
JosephWong <joseph_wong1993@foxmail.com>
2022-05-13 21:44:04 +0800

    study SpringBoot

--- 黑马程序员2020年Java就业班视频/07-SpringBoot/springboot/springboot-06-rest-template/src/test/java/com/wong/Springboot06RestTemplateApplicationTests.java
+++ 黑马程序员2020年Java就业班视频/07-SpringBoot/springboot/springboot-06-rest-template/src/test/java/com/wong/Springboot06RestTemplateApplicationTests.java
@@ -2,15 +2,24 @@ package com.wong;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
+/**
+ * @SpringBootTest 代表当前类是一个SpringBoot的测试类
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Springboot06RestTemplateApplicationTests {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @Test
     public void contextLoads() {
+        String forObject = restTemplate.getForObject("http://baidu.com", String.class);
+        System.out.println(forObject);
     }
-
 }
