1b938bce172146dcbd57e04d7ef146c733153bd6
joseph <joseph9312@qq.com>
2022-10-27 20:34:00 +0800

    study

---
+++ 黑马程序员2020年Java就业班视频/08-ChangGou/02/wong-web-test/src/main/java/com/wong/WongWebTestApplication.java
@@ -0,0 +1,11 @@
+package com.wong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WongWebTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WongWebTestApplication.class, args);
+    }
+}
