10c960f949b7aa402dced4ce18bf0646cc6c553c
joseph <joseph_wong1993@foxmail.com>
2023-04-21 17:21:41 +0800

    变更

--- 黑马程序员2020年Java就业班视频/07-SrpingCould/wong-spring-cloud-parent/provider-service/src/main/java/com/wong/controller/UserController.java
+++ 黑马程序员2020年Java就业班视频/07-SrpingCould/wong-spring-cloud-parent/provider-service/src/main/java/com/wong/controller/UserController.java
@@ -2,19 +2,23 @@ package com.wong.controller;
 
 import com.wong.pojo.User;
 import com.wong.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@RefreshScope // 刷新配置
 public class UserController {
     private final UserService userService;
 
     @Value("${server.port}")
     private String port;
+
+    @Value("${test.hello}")
+    private String name;
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -23,17 +27,15 @@ public class UserController {
     /**
      * 根据id查询
      */
-    @RequestMapping("/query_by_id")
-    public User queryById(@RequestParam("id") Integer id) throws InterruptedException {
+    @RequestMapping("/findById")
+    public User findById(@RequestParam("id") Integer id) throws InterruptedException {
 //        Thread.sleep(3000);
         // 手动抛出异常
         if (id == 1) {
             throw new RuntimeException("啊，我死掉了！！！");
         }
         User user = userService.queryById(id);
-        user.setNote("当前服务端口：" + port);
+        user.setNote("当前服务端口：" + port + "; name = " + name);
         return user;
     }
-
-
 }
